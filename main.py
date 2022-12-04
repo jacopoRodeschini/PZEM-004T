03009a07e336dcb4949c37f66d1ed6c569a23cba
j.rodeschini <j.rodeschini@studenti.unibg.it>
2022-12-05 00:33:31 +0100

    Add example (main.py)

---
+++ main.py
@@ -0,0 +1,54 @@
+# *---------------------------------------------*
+# | PZEM (v3.0)                                 |
+# |                                             |
+# | Micropython driver to communicate with      |
+# | PZEM-004T (v0.3) using Modbus-RTU protocol  |
+# | on the hardware UART port                   |
+# |                                             |
+# | Authors: Jacopo Rodeschini                  |
+# | License: GPLv3                              |
+# *---------------------------------------------*
+
+# PZEM is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# PZEM is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with PZEM. If not, see <http://www.gnu.org/licenses/>.
+
+# main.py file
+
+from pzem import PZEM
+from machine import UART, time
+
+# define spleeping time [sec.]
+sleep = 60
+
+# define hardware uart
+uart = machine.UART(1, baudrate=9600, timeout=500)
+
+# define PZEM device [UART, ADDR = 0xF8 (default)]
+dev = PZEM(uart=uart)
+
+# Set new address
+if dev.setAddr(0x05):
+    print("New device address is {}".format(dev.getAddr()))
+
+
+while True:
+
+    # Read the new values
+    if dev.read():
+
+        # print the reading value (public filed)
+        print(dev.toString())
+        print(dev.getCurrent())
+
+    # wait for the next reading
+    time.sleep(sleep - dev.getReadingTime())
